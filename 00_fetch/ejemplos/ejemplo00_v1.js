ae14b89f2c4b02cf4b2e45c9adb84f2a95afe85f
Luis Antonio Peña Murillo <apmurillo99@hotmail.com>
2022-06-12 05:10:02 -0500

    feat: Update files

--- 00_fetch/ejemplos/ejemplo00_v1.js
+++ 00_fetch/ejemplos/ejemplo00_v1.js
@@ -1,14 +1,11 @@
 console.log('Metodo fetch promise');
 let url = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/mxn.json';
 const peticion = fetch(url)
-.then(response => {
-    console.log(response); 
-    response.json().then(
-        data => {
-            console.log(data)
-            return data;
-        }
-     )
+    .then(response => {
+        console.log(response);
+        response.json().then(
+            data => console.log(data)
+        )
     }
 )
 peticion;
